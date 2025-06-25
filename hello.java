441547da76160e1a369fcb4c4e936fa8859afce4
Harish005-Dev <haridy009@gmail.com>
2025-06-25 17:06:58 +0530

    changes accpet of both party

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from Java!");
+        System.out.println(new java.util.Date());
+    }
+}
---
+++ hello.py
@@ -0,0 +1,4 @@
+import datetime
+print("Hello, World from Python!")
+s = datetime.datetime.now()
+print(s.strftime("%Y-%m-%d %H:%M:%S"))
