fbe03bedda29a8c32e8be0af2fe2e51b8e94acb8
Harish005-Dev <haridy009@gmail.com>
2025-07-01 18:00:49 +0530

    Some change done js file

--- jswebservice.js
+++ jswebservice.js
@@ -4,4 +4,13 @@ app.get("/", function (request, response) {
     response.send("Hello World " + new Date().toLocaleDateString());
 
 })
+app.get("/hello", function (request, response) {
+    response.send("Hello World .how are you" + new Date().toLocaleDateString());
+
+})
+app.get("/emp", function (request, response) {
+    data = [{ "empid": 1, "ename": "abc", "salary": 2000000 }, { "empid": 2, "ename": "abc", "salary": 2200000 }, { "empid": 3, "ename": "abc", "salary": 2050000 }]
+    response.send(data);
+
+})
 app.listen(8989);
